520599b2adc9a0e767a3975007f6c9fbceb7fcc6
JuanQuezada1 <98851871+JuanQuezada1@users.noreply.github.com>
2022-06-03 17:07:06 -0400

    Primer Hola Mundo

---
+++ Sesion1/src/test/java/cl/holamundo/java/HolamundospringApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.holamundo.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolamundospringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
