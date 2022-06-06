043dc8b193c16c8c95bd7408da951068a1b680aa
JuanQuezada1 <98851871+JuanQuezada1@users.noreply.github.com>
2022-06-05 21:31:23 -0400

    Delete REST mod1 directory

--- REST mod1/src/main/java/cl/aiep/java/rest/exceptions/ProductoNoEncontradoException.java
+++
@@ -1,8 +0,0 @@
-package cl.aiep.java.rest.exceptions;
-
-public class ProductoNoEncontradoException extends RuntimeException{
-
-	public ProductoNoEncontradoException(Long id) {
-		super(String.format("Producto con id %s no encontrado",id));
-	}
-}
--- REST mod1/src/main/java/cl/aiep/java/rest/repository/ProductoRepository.java
+++
@@ -1,9 +0,0 @@
-package cl.aiep.java.rest.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import cl.aiep.java.rest.model.Producto;
-
-public interface ProductoRepository extends JpaRepository<Producto, Long> {
-
-}
