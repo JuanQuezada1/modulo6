d8407f103b4d62ffcfe20fe9835a79d9c1b08900
JuanQuezada1 <98851871+JuanQuezada1@users.noreply.github.com>
2022-06-05 20:04:07 -0400

    uso de lombok

---
+++ Lombok/src/test/java/cl/aiep/java/lombok/LombokApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.aiep.java.lombok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LombokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
