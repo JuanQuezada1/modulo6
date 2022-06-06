5fa1513c0c78baea543fa5912e6dbdb1bf4f0a43
JuanQuezada1 <98851871+JuanQuezada1@users.noreply.github.com>
2022-06-05 21:29:26 -0400

    Catalogo REST

---
+++ Sesion16 REST/src/main/java/cl/aiep/java/rest/repository/ProductoRepository.java
@@ -0,0 +1,9 @@
+package cl.aiep.java.rest.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import cl.aiep.java.rest.model.Producto;
+
+public interface ProductoRepository extends JpaRepository<Producto, Long>{
+
+}
