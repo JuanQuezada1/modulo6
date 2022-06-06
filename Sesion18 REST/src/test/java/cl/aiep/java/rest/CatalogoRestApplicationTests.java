64ac204552a2aace4ad3a8dde325f94757962237
JuanQuezada1 <98851871+JuanQuezada1@users.noreply.github.com>
2022-06-05 21:32:21 -0400

    Envio de datos

---
+++ Sesion18 REST/src/test/java/cl/aiep/java/rest/CatalogoRestApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.aiep.java.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogoRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
