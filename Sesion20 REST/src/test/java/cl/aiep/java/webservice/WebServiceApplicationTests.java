923b3b9c56906a92ac2fc68b07f84f50959b78f5
JuanQuezada1 <98851871+JuanQuezada1@users.noreply.github.com>
2022-06-05 21:34:52 -0400

    Rest Web Service

---
+++ Sesion20 REST/src/test/java/cl/aiep/java/webservice/WebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.aiep.java.webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
